60563696d959aa879f4897a99ed286e7ca62b60e
blackmonkey121 <3213322480@qq.com>
2019-12-15 23:18:50 +0800

    add models for apps

--- djangoBlog/apps/user/views.py
+++ djangoBlog/apps/user/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 
 
-def login():
-    return 'OK'
+def index(request):
+    return HttpResponse("Anything is OK!")
--- djangoBlog/djangoBlog/settings/develop.py
+++ djangoBlog/djangoBlog/settings/develop.py
@@ -1,2 +1,13 @@
 from .base import *    # No Q/A
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoBlog',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'monkey123',
+    }
+}
